b448c8d48270508b8ee490eeb1b2a4e1126d10e9
Nishanov Khasan <99598107+HasanNishanov@users.noreply.github.com>
2022-07-21 09:35:01 +0500

    Have a good day

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if(first > second){
+            System.out.println(first);
+        }
+        if(first < second){
+            System.out.println(second);
+        }
+        else if(first == second){
+            System.out.println(first);
+        }
+    }
+
+    public static void main(String[] args) {
+        GreatestNumberPrinter greatestNumberPrinter = new GreatestNumberPrinter();
+        greatestNumberPrinter.printGreatest(12,-400);
     }
 }
